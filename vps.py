127054d7e677358260608d5ae4a0dea34958d5ea
unknown <jacek.pardyak@gmail.com>
2021-09-04 22:34:33 +0200

    part 5

--- vps.py
+++ vps.py
@@ -2,8 +2,11 @@ import pandas as pd
 
 df = pd.read_csv(r'data/vps_churn_data.txt')
 print (df)
-df.to_json (r'data/vps_churn_data_py.json')
+df.to_json (r'data/vps_churn_data_py.json', orient='records')
 
 df = pd.read_csv(r'data/vps_test_data.txt')
 print (df)
-df.to_json (r'data/vps_test_data_py.json')
+df.to_json (r'data/vps_test_data_py.json', orient='records')
+
+df = pd.read_json(r'data/vps_churn_data_py.json')
+print (df)
