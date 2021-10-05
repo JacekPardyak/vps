12015ee7cf96af79f0abbcb7ecec05538d6de510
unknown <jacek.pardyak@gmail.com>
2021-10-05 10:42:19 +0200

    clean

--- data-abalone.R
+++
@@ -1,4 +0,0 @@
-library(AppliedPredictiveModeling)
-library(tidyverse)
-data("abalone")
-abalone %>% write_csv('./Mastering Machine Learning with Spark 3.x/abalone.csv')
---
+++ query.sql
@@ -0,0 +1,5 @@
+-- !preview conn=sc
+
+--SELECT * FROM vps WHERE id > 106 LIMIT 10;
+
+SELECT * FROM pro WHERE id > 10 LIMIT 10; --  id > 106
