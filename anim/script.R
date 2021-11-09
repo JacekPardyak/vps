3a58b6be5831d334fbe9a55438fb87d74ea9c654
unknown <jacek.pardyak@gmail.com>
2021-11-09 20:25:28 +0100

    anim

---
+++ anim/script.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(sf)
+library(gganimate)
+
+data <- st_read("https://raw.githubusercontent.com/JacekPardyak/vps/master/data/logo/logo.dxf")
+data
+layers <- data %>% data.frame() %>% distinct(Layer) %>% pull()
+layers
+
+# Layer 1
+layer <- layers[1]
+layer_1 <- data %>% filter(Layer == layer) %>% st_union() %>% st_polygonize() %>% first()  %>% first()
+layer_1 <- layer_1  %>%
+  st_sfc() %>% st_sf(geometry = .) %>% mutate(Layer = layer)
+layer_1
+layer_1 %>% plot()
+
+# Layer 2
+layer_in  <- layers[2]
+layer_out <- layers[3]
+
+layer_2_in  <- data %>% filter(Layer == layer_in) %>% st_union() %>% st_polygonize() 
+layer_2_out <- data %>% filter(Layer == layer_out) %>% st_union() %>% st_polygonize() 
+
+layer_2 <- st_difference(layer_2_out, layer_2_in)
+layer_2 <- layer_2  %>%
+  st_sfc() %>% st_sf(geometry = .) %>% mutate(Layer = layer)
+layer_2 %>% plot()
+
+layer_2
+
+# Layer 3
+layer = "Layer 3"
+layer_in  <- layers[4]
+layer_out <- layers[5]
+
+layer_in  <- data %>% filter(Layer == layer_in) %>% st_union() %>% st_polygonize() 
+layer_out <- data %>% filter(Layer == layer_out) %>% st_union() %>% st_polygonize() 
+
+layer_3 <- st_difference(layer_out, layer_in)
+layer_3 <- layer_3  %>%
+  st_sfc() %>% st_sf(geometry = .) %>% mutate(Layer = layer)
+layer_3 %>% plot()
+
+
+## data
+data <- (layer_2 %>% st_sample(size = 1000) %>%
+           st_sfc() %>% st_sf(geometry = .) %>% mutate(Facet = 1) %>% mutate(Colour = "#d51125") ) %>% 
+  bind_rows(layer_2 %>% st_sample(size = 1000) %>%
+              st_sfc() %>% st_sf(geometry = .) %>% mutate(Facet = 2)  %>% mutate(Colour = "#cc1100") )
+
+data %>% ggplot() +
+  aes(colour = Colour, fill = Colour) +
+  geom_sf(aes(group = 1L)) +
+  scale_colour_identity() +
+  scale_fill_identity() +
+  theme_void() +
+  transition_states(Facet, wrap = FALSE) #+  shadow_mark() 
