9d3423f99606974e3711bc5cd9b2424d94623ad0
unknown <jacek.pardyak@gmail.com>
2021-12-06 23:35:22 +0100

    magic

--- shiny-pong-working/pong_py/run.bat
+++
@@ -1,8 +0,0 @@
-ECHO OFF
-ECHO Hello World
-<<<<<<< HEAD
-python C:\\Users\\jacek\\OneDrive\\Documents\\vps\\shiny-pong-working\\pong_py\\ball.py
-=======
-C:/Users/Public/r-minicondapython "C:\Users\Jacek Pardyak\Documents\vps\shiny-pong-working\pong_py\noisy.py"
->>>>>>> 2a0cddf5246c876d7195d58f499d0d758e7b0dfb
-PAUSE
