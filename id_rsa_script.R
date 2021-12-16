a9380ca2d123a05b35ee65c7b96d06b17fcd0902
unknown <jacek.pardyak@gmail.com>
2021-12-16 20:35:13 +0100

    f**k $

---
+++ id_rsa_script.R
@@ -0,0 +1,8 @@
+library(encryptr)
+files <- list.files(path = "./my_space/", full.names = T)
+for (file in files) {
+  encrypt_file(file)
+  
+}
+
+decrypt_file("./my_space/shiny-survival.zip.encryptr.bin")
