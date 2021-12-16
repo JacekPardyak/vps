04718046ca79daaec89ee98c0be5d803ec7e5b2a
unknown <jacek.pardyak@gmail.com>
2021-12-16 20:23:42 +0100

    clean

--- my_space/shiny-survival/my_space.R
+++
@@ -1,9 +0,0 @@
-library(encryptr)
-genkeys()
-
-write.csv(gp, "gp.csv")
-
-encrypt_file("gp.csv")
-
-
-decrypt_file("gp.csv.encryptr.bin", file_name = "gp2.csv")
--- my_space/shiny-survival/shiny-survival-analysis - 3y-home/global.R
+++
@@ -1,8 +0,0 @@
-library(survival)
-library(tidyverse)
-library(tidymodels)
-
-df <- read_csv("./out_3y.csv") %>% filter(PG_2 != '?') 
-
-prod_all <- df %>% select(all_of("PROD")) %>% distinct() %>% pull()
-
