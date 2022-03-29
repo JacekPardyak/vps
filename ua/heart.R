57e850d4532407bf3116f8c5c7df210d73d4c14b
unknown <jacek.pardyak@gmail.com>
2022-03-29 18:54:00 +0200

    vps

--- ua/heart.R
+++ ua/heart.R
@@ -29,32 +29,33 @@ heart %>% st_write("./ua/Wikimedia_Community_Logo-WOSP.shp")
 victory <- st_read("./ua/victory.dxf") 
 d1 <- victory %>% slice(17:178) %>% st_union() %>% st_polygonize() %>%
   first() %>% first() %>% st_geometry() %>%
-  st_sfc()
-victory %>%  
-  ggplot() + 
-    geom_sf() +
-    theme_void()
+  st_sfc() %>% st_as_sf() %>%
+  mutate(facet = 0) %>% mutate(colour = "#000000")
   
 d2 <- victory %>% slice(179:308) %>% st_union() %>% st_polygonize() %>%
   first() %>% first() %>% st_geometry() %>%
-  st_sfc()
-d2 %>%  
-  ggplot() + 
-  geom_sf() +
-  theme_void()
+  st_sfc() %>% st_as_sf() %>%
+  mutate(facet = 0) %>% mutate(colour = "#000000")
 
 d3 <- victory %>% slice(309:337) %>% st_union() %>% st_polygonize() %>%
   first() %>% first() %>% st_geometry() %>%
-  st_sfc()
-d3 %>%  
-  ggplot() + 
-  geom_sf() +
-  theme_void()
+  st_sfc() %>% st_as_sf() %>%
+  mutate(facet = 0) %>% mutate(colour = "#005BBB")
 
 d4 <- victory %>% slice(338:366) %>% st_union() %>% st_polygonize() %>%
   first() %>% first() %>% st_geometry() %>%
-  st_sfc()
-d4 %>%  
+  st_sfc() %>% st_as_sf() %>%
+  mutate(facet = 0) %>% mutate(colour = "#FFD500")
+
+data <- d1 %>% bind_rows(d2) %>% bind_rows(d3) %>% bind_rows(d4)
+
+data %>%
   ggplot() + 
-  geom_sf() +
-  theme_void()
+  aes(colour = colour, fill = colour) +
+  geom_sf(aes(group = 1L)) +
+  scale_colour_identity() +
+  scale_fill_identity() +
+  theme_void() + 
+  theme(panel.background = element_rect(fill = 'white'))
+
+ggsave("./ua/victory.png")
